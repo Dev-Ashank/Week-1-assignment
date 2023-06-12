259007f94ad23fe9d7e702fca8a73ecb3f990d73
Dev-ashank <kumardivyashankgaurav@gmail.com>
2023-06-12 11:54:37 +0530

    expenditure-analysis done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,8 +8,48 @@
   - `npm run test-expenditure-analysis`
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+// Create an array to store transactions
+const transactions = [];
+
+// Function to add a transaction to the list
+function addTransaction(itemName, category, price) {
+  const transaction = {
+    itemName: itemName,
+    category: category,
+    price: price,
+    timestamp: new Date(),
+  };
+  transactions.push(transaction);
 }
+
+// Example usage
+addTransaction("Shoes", "Clothing", 49.99);
+addTransaction("Tshirts","Clothing",20.00);
+addTransaction("Headphones", "Electronics", 79.99);
+addTransaction("Samrtphones","Electronics",400.00);
+addTransaction("Book", "Books", 19.99);
+
+// Display the list of transactions
+console.log(transactions);
+
+
+
+function calculateTotalSpentByCategory(transactions) {
+  const categoryPrices = [];
+  for(let transaction of transactions) {
+    const{category,price} = transaction;
+    if(categoryPrices[category])
+    {
+      categoryPrices[category] += price;
+    }
+    else{
+      categoryPrices[category] = price;
+    }
+  }
+
+
+  return categoryPrices;
+}
+console.log(calculateTotalSpentByCategory(transactions));
 
 module.exports = calculateTotalSpentByCategory;
